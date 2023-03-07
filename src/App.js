a439b78ceca2b3cbf9ff2a94e0e71c208fea9c3e
Rahul <rahulsonwalkar23@gmail.com>
2023-03-06 19:49:23 -0800

    use SVG

--- src/App.js
+++ src/App.js
@@ -68,6 +68,9 @@ function Examples(props) {
               <p className="text-sm font-medium text-gray-900">{q}</p>
             </a>
           </div>
+          <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+              <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
+            </svg>
         </div>
       ))}
     </div>
