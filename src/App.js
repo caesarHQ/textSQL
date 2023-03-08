ee865c0c08973efa2b134dd2fe68af31e18f42bd
Rahul <rahulsonwalkar23@gmail.com>
2023-03-07 19:54:29 -0800

    split examples

--- src/App.js
+++ src/App.js
@@ -56,34 +56,67 @@ function LoadingSpinner(props) {
 }
 
 function Examples(props) {
-  const example_queries = [
-    "3 neighborhoods in San Francisco that have the highest female to male ratio",
-    "Which 5 areas have the median income closest to the national median income?",
+  const basic_example_queries = [
     "Five cities in Florida with the highest crime",
     "Richest neighborhood in Houston, TX"
   ]
+  const advanced_example_queries = [
+    "3 neighborhoods in San Francisco that have the highest female to male ratio",
+    "Which area in San Francisco has the highest racial diversity and what is the racial distribution of that area",
+    "Which 5 areas have the median income closest to the national median income?"
+  ]
   return (
-    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-      {example_queries.map((q) => (
-        <div
-          key={q}
-          className="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
-        >
-          <div className="min-w-0 flex-1">
-            <a className="focus:outline-none" onClick={() => {
-                posthog.capture('example_clicked', { natural_language_query: q })
-                props.setQuery(q)
-                props.handleClick(q)
-              }}>
-              <span className="absolute inset-0" aria-hidden="true" />
-              <p className="text-sm font-medium text-gray-900">{q}</p>
-            </a>
+    <div> 
+      <div >
+        <p className="my-4"> Basic </p>
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+        {basic_example_queries.map((q) => (
+          <div
+            key={q}
+            className="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
+          >
+            <div className="min-w-0 flex-1">
+              <a className="focus:outline-none" onClick={() => {
+                  posthog.capture('example_clicked', { natural_language_query: q })
+                  props.setQuery(q)
+                  props.handleClick(q)
+                }}>
+                <span className="absolute inset-0" aria-hidden="true" />
+                <p className="text-sm font-medium text-gray-900">{q}</p>
+              </a>
+            </div>
+            <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24">
+                <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
+              </svg>
           </div>
-          <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-              <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
-            </svg>
+        ))}
         </div>
-      ))}
+      </div>
+      <div >
+        <p className="my-4"> Advanced </p>
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+        {advanced_example_queries.map((q) => (
+          <div
+            key={q}
+            className="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
+          >
+            <div className="min-w-0 flex-1">
+              <a className="focus:outline-none" onClick={() => {
+                  posthog.capture('example_clicked', { natural_language_query: q })
+                  props.setQuery(q)
+                  props.handleClick(q)
+                }}>
+                <span className="absolute inset-0" aria-hidden="true" />
+                <p className="text-sm font-medium text-gray-900">{q}</p>
+              </a>
+            </div>
+            <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24">
+                <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
+              </svg>
+          </div>
+        ))}
+        </div>
+      </div>
     </div>
   )
 }
@@ -373,7 +406,6 @@ function App() {
       }
     }
   })
-
 
 const zipcodeLayerLow =   {
     'id': 'zips-kml',
