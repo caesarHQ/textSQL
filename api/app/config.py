1ec93d6012c73ee06991b4580ff2711e010e4bc4
Nicholas <contactnicholas81@gmail.com>
2023-03-10 19:09:23 +0000

    add dotenv to config.py

--- api/app/config.py
+++ api/app/config.py
@@ -1,6 +1,8 @@
 from os import getenv
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
 
+load_dotenv()
 
 DB_URL = "postgres://census_data_user:BvgUaxoocxdDrJ9Mam4HHkacPBWLYYt9@dpg-cg59te3hp8u9l20dqd40-b.replica-cyan.oregon-postgres.render.com/census_data"
 OPENAI_KEY = getenv("OPENAI_KEY")
