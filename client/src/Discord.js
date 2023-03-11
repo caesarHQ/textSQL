6949fc4f45945aaa3bffa98d55640ee8fe0e3ba0
tanujtiwari98 <tanujtiwari98@gmail.com>
2023-03-10 16:28:56 -0800

    add prettier to the project

--- client/src/Discord.js
+++ client/src/Discord.js
@@ -1,24 +1,31 @@
-import {BsDiscord, BsGithub} from "react-icons/bs";
+import { BsDiscord, BsGithub } from 'react-icons/bs'
 
 export const DiscordButton = () => {
-     return <button
-        type="button"
-        className="inline-flex items-center gap-x-1.5 rounded-md bg-[#ebf0f4] py-1.5 px-2.5 text-sm font-semibold text-[#24292f] shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 "
-        onClick={() => window.open('https://discord.gg/JZtxhZQQus','_blank')}
-      >
-         <BsDiscord />
-        Discord
-      </button>
-
+    return (
+        <button
+            type="button"
+            className="inline-flex items-center gap-x-1.5 rounded-md bg-[#ebf0f4] py-1.5 px-2.5 text-sm font-semibold text-[#24292f] shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 "
+            onClick={() =>
+                window.open('https://discord.gg/JZtxhZQQus', '_blank')
+            }
+        >
+            <BsDiscord />
+            Discord
+        </button>
+    )
 }
 // "https://github.com/caesarhq/textSQL"
 export const GithubButton = () => {
-         return <button
-        type="button"
-        className="inline-flex items-center gap-x-1.5 rounded-md bg-[#ebf0f4] py-1.5 px-2.5 text-sm font-semibold text-[#24292f] shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 "
-        onClick={() => window.open('https://github.com/caesarhq/textSQL','_blank')}
-      >
-         <BsGithub />
-        Star on GitHub
-      </button>
+    return (
+        <button
+            type="button"
+            className="inline-flex items-center gap-x-1.5 rounded-md bg-[#ebf0f4] py-1.5 px-2.5 text-sm font-semibold text-[#24292f] shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 "
+            onClick={() =>
+                window.open('https://github.com/caesarhq/textSQL', '_blank')
+            }
+        >
+            <BsGithub />
+            Star on GitHub
+        </button>
+    )
 }
