440baaac5020b02ba75346b5bea074f09af26cb4
tanujtiwari98 <tanujtiwari98@gmail.com>
2023-03-12 18:02:07 -0700

    first rephrase the query

--- api/app/api/utils/text_to_sql.py
+++ api/app/api/utils/text_to_sql.py
@@ -107,11 +107,12 @@ def make_rephrase_msg_with_schema_and_warnings(table_names: List[str]):
             "---------------------\n" + generate_msg_with_schemas(table_names) +
             "\n\n"
             "---------------------\n"
+            "Do not include any of the tabel names in the query. Just try to guess what a data analyst would structure the natural language query as if it knew these tables existed."
     )
 
 def make_msg_with_schema_and_warnings(table_names: List[str]):
     return (
-            "Generate syntactically correct read-only SQL to answer the cleaned up query generated above"
+            "Generate syntactically correct read-only SQL to answer the following question/command: {natural_language_query}"
             "The following are schemas of tables you can query:\n"
             "---------------------\n" + generate_msg_with_schemas(table_names) +
             "\n\n"
@@ -297,18 +298,15 @@ def text_to_sql_with_retry(natural_language_query, table_names, k=3, messages=No
     Tries to take a natural language query and generate valid SQL to answer it K times
     """
     if not messages:
-        content = make_msg_with_schema_and_warnings(table_names).format(natural_language_query=natural_language_query)
-        messages = make_default_messages(table_names).copy()
         # ask the assistant to rephrase before generating the query
-        messages.append({
+        rephrase = [{
             "role": "user",
-            "content": make_rephrase_msg_with_schema_and_warnings(table_names).format(natural_language_query=natural_language_query)
-        })
-        assistant_message = get_assistant_message(messages)
-        messages.append({
-            "role": "assistant",
-            "content": assistant_message['message']['content']
-        })
+            "content": make_rephrase_msg_with_schema_and_warnings(table_names).format(
+                natural_language_query=natural_language_query)
+        }]
+        assistant_message = get_assistant_message(rephrase)
+        content = make_msg_with_schema_and_warnings(table_names).format(natural_language_query=assistant_message['message']['content'])
+        messages = make_default_messages(table_names).copy()
         messages.append({
             "role": "user",
             "content": content
