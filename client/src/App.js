9b3286108d4d7fe7e414712b5a8d4c8e801ed087
noahvember <noahvember@pm.me>
2023-03-13 03:16:39 -0500

    rm unused imp

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect, Fragment } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import Map, { Layer, Source } from 'react-map-gl'
 import mapboxgl from 'mapbox-gl'
 import bbox from '@turf/bbox'
