c3077007567d64354b046ab9b8855a3b744af8ef
tanujtiwari98 <tanujtiwari98@gmail.com>
2023-03-13 22:10:33 -0700

    hide disclaimer on mobile

--- client/src/components/disclaimer.js
+++ client/src/components/disclaimer.js
@@ -1,6 +1,6 @@
 const Disclaimer = () => {
     return (
-        <div className="flex items-center mt-2 justify-center">
+        <div className="hidden flex items-center mt-2 justify-center md:block">
             <p className="text-xs tracking-tight text-gray-600 dark:text-white">
                 Disclaimer: CensusGPT currently only supports data about crime,
                 age, race, gender, income, and population in the USA. But we are
