52212a2ce675cf7bf1522a32be9688286a548cef
alex <alex@caesarhq.com>
2023-03-14 14:44:41 -0700

    remove table schemas in system message (already included in user message)

--- api/app/api/utils/table_selection.py
+++ api/app/api/utils/table_selection.py
@@ -24,24 +24,6 @@ DEFAULT_MESSAGES = [
                 " You respond in JSON format with your answer in a field named \"tables\" which is a list of strings."
                 " Respond with an empty list if you cannot identify any relevant tables."
                 " Write your answer in markdown format."
-                "\n"
-                "The following are descriptions of available tables:\n"
-                "---------------------\n"
-                "Description of table 'crime_by_city':\n"
-                "Table 'crime_by_city' has city-level crime data in the United States."
-                " It has numbers for violent crime, murder and nonnegligent manslaughter, rape, robbery, aggravated assault, property crime, burglary, larceny theft, motor vehicle theft, and arson."
-                "Table 'crime_by_city' has columns: city (TEXT), violent_crime (DOUBLE_PRECISION), murder_and_nonnegligent_manslaughter (DOUBLE_PRECISION), rape (DOUBLE_PRECISION), robbery (DOUBLE_PRECISION), aggravated_assault (DOUBLE_PRECISION), property_crime (DOUBLE_PRECISION), burglary (DOUBLE_PRECISION), larceny_theft (DOUBLE_PRECISION), motor_vehicle_theft (DOUBLE_PRECISION), arson (DOUBLE_PRECISION), state (TEXT)."
-
-                "\n\n"
-                "Description of table 'demographic_data':\n"
-                "Table 'demographic_data' has zip-code level data about average household sizes, income, and population grouped by gender, age, education levels, and race."
-                "Table 'demographic_data' has columns: total_population (DOUBLE_PRECISION), elderly_population (DOUBLE_PRECISION), male_population (DOUBLE_PRECISION), female_population (DOUBLE_PRECISION), white_population (DOUBLE_PRECISION), black_population (DOUBLE_PRECISION), native_american_population (DOUBLE_PRECISION), asian_population (DOUBLE_PRECISION), two_or_more_population (DOUBLE_PRECISION), hispanic_population (DOUBLE_PRECISION), adult_population (DOUBLE_PRECISION), citizen_adult_population (DOUBLE_PRECISION), average_household_size (DOUBLE_PRECISION), population_under_5_years (DOUBLE_PRECISION), population_5_to_9_years (DOUBLE_PRECISION), population_10_to_14_years (DOUBLE_PRECISION), population_15_to_19_years (DOUBLE_PRECISION), population_20_to_24_years (DOUBLE_PRECISION), population_25_to_34_years (DOUBLE_PRECISION), population_35_to_44_years (DOUBLE_PRECISION), population_45_to_54_years (DOUBLE_PRECISION), population_55_to_59_years (DOUBLE_PRECISION), population_60_to_64_years (DOUBLE_PRECISION), population_65_to_74_years (DOUBLE_PRECISION), population_75_to_84_years (DOUBLE_PRECISION), population_85_years_and_over (DOUBLE_PRECISION), per_capita_income (DOUBLE_PRECISION), median_income_for_workers (DOUBLE_PRECISION), zip_code (TEXT), city (TEXT), state (TEXT), county (TEXT), lat (DOUBLE_PRECISION), lon (DOUBLE_PRECISION)"
-                ", no_schooling_completed (DOUBLE_PRECISION), nursery_school (DOUBLE_PRECISION), kindergarten (DOUBLE_PRECISION), grade_1 (DOUBLE_PRECISION), grade_2 (DOUBLE_PRECISION), grade_3 (DOUBLE_PRECISION), grade_4 (DOUBLE_PRECISION), grade_5 (DOUBLE_PRECISION), grade_6 (DOUBLE_PRECISION), grade_7 (DOUBLE_PRECISION), grade_8 (DOUBLE_PRECISION), grade_9 (DOUBLE_PRECISION), grade_10 (DOUBLE_PRECISION), grade_11 (DOUBLE_PRECISION), grade_12_no_diploma (DOUBLE_PRECISION),"
-                ", regular_high_school_diploma (DOUBLE_PRECISION), ged_or_alternative_credential (DOUBLE_PRECISION), some_college_less_than_1_year (DOUBLE_PRECISION), some_college_1_or_more_years_no_degree (DOUBLE_PRECISION), associates_degree (DOUBLE_PRECISION), bachelors_degree (DOUBLE_PRECISION)"
-                ", masters_degree (DOUBLE_PRECISION), professional_school_degree (DOUBLE_PRECISION), doctorate_degree (DOUBLE_PRECISION)"
-                "."
-                "\n\n"
-                "---------------------\n"
             )
     },
     {
