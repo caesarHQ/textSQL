1387140016de31fd9c7bd4d9be6f3392cde7ae81
alex <alex@caesarhq.com>
2023-03-15 13:00:55 -0700

    use json to load few shot examples

---
+++ api/app/api/utils/few_shot_examples.py
@@ -0,0 +1,22 @@
+import json
+from typing import List
+
+
+few_shot_examples = {}
+with open("app/data/few_shot_examples.json", "r") as f:
+    few_shot_examples = json.load(f)
+
+
+def get_few_shot_example_messages(scope: str = "general") -> str:
+    examples = few_shot_examples.get(scope)
+    messages = []
+    for example in examples:
+        messages.append({
+            "role": "user",
+            "content": example["natural_language_query"],
+        })
+        messages.append({
+            "role": "assistant",
+            "content": example["sql"],
+        })
+    return messages
