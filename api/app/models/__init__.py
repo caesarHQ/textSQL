c001b50db6ab126dd99ac7c1741dc10da4877b06
alex <alex@caesarhq.com>
2023-03-15 14:21:33 -0700

    remove unused /models

--- api/app/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from .acs_5_yr_2021 import AcsFiveYear2021
-from .geo_data import GeoData
-from .crime import CrimeByCity
-
-__all__ = [
-    'AcsFiveYear2021',
-    'GeoData',
-    'CrimeByCity',
-    ]
