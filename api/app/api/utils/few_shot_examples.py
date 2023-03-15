4c3d92b9219175a7e3635b768471c845008715f0
alex <alex@caesarhq.com>
2023-03-15 13:19:21 -0700

    load json for few shot examples on table selection

--- api/app/api/utils/few_shot_examples.py
+++ api/app/api/utils/few_shot_examples.py
@@ -7,16 +7,16 @@ with open("app/data/few_shot_examples.json", "r") as f:
     few_shot_examples = json.load(f)
 
 
-def get_few_shot_example_messages(scope: str = "general") -> List[dict]:
+def get_few_shot_example_messages(scope: str = "general_text_to_sql") -> List[dict]:
     examples = few_shot_examples.get(scope)
     messages = []
     for example in examples:
         messages.append({
             "role": "user",
-            "content": example["natural_language_query"],
+            "content": example["user"],
         })
         messages.append({
             "role": "assistant",
-            "content": example["sql"],
+            "content": example["assistant"],
         })
     return messages
