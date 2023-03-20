9ca2b4526f25dd505ff84e7bacc90005297e7388
alex <alex@caesarhq.com>
2023-03-20 14:33:26 -0700

    wip custom type prompting

--- api/app/api/utils/table_details.py
+++ api/app/api/utils/table_details.py
@@ -1,5 +1,6 @@
 import json
 from typing import List
+import re
 
 
 table_details = {}
@@ -11,7 +12,20 @@ with open("app/data/sf_tables.json", "r") as f:
     sf_table_details = json.load(f)
 
 
+def extract_text_from_markdown(text):
+    regex = r"`([\s\S]+?)`"
+    matches = re.findall(regex, text)
+
+    if matches:
+        extracted_text = matches[0]
+    else:
+        extracted_text = text
+
+    return extracted_text
+
+
 def get_table_schemas(table_names: List[str] = None, scope="USA") -> str:
+    custom_types_list = []
     tables_list = []
 
     if table_names:
@@ -19,23 +33,33 @@ def get_table_schemas(table_names: List[str] = None, scope="USA") -> str:
             if table['name'] in table_names:
                 tables_list.append(table)
     elif scope == "USA":
-        tables_list = table_details['tables']
+        custom_types_list = table_details.get('types', [])
+        tables_list = table_details.get('tables', [])
     elif scope == "SF":
-        tables_list = sf_table_details['tables']
+        custom_types_list = sf_table_details.get('types', [])
+        tables_list = sf_table_details.get('tables', [])
 
-    # return json.dumps(tables_list, indent=4)
+    custom_types_str_list = []
+    for custom_type in custom_types_list:
+        custom_types_str = f"custom type: {custom_type['type']}\n"
+        custom_types_str += f"valid values: {', '.join(custom_type['valid_values'])}\n"
+        custom_types_str_list.append(custom_types_str)
+    custom_types_description = "\n\n".join(custom_types_str_list)
+
     tables_str_list = []
     for table in tables_list:
         tables_str = f"table name: {table['name']}\n"
         tables_str += f"table description: {table['description']}\n"
         columns_str_list = []
         for column in table['columns']:
-            # columns_str_list.append(f"{column['name']} ({column['type']})")
             if column.get('description'):
                 columns_str_list.append(f"{column['name']} [{column['type']}] ({column['description']})")
+                if 'custom type' in column['description']:
+                    custom_type = extract_text_from_markdown(column['description'])
             else:
                 columns_str_list.append(f"{column['name']} [{column['type']}]")
         tables_str += f"table columns: {', '.join(columns_str_list)}\n"
         tables_str_list.append(tables_str)
-    
-    return "\n\n".join(tables_str_list)
+    tables_description = "\n\n".join(tables_str_list)
+
+    return tables_description
