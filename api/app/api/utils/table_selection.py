9d158e97ba13f484b35dff035c114ea16068eea3
alex <alex@caesarhq.com>
2023-03-20 00:10:00 -0700

    add scope/mode  + SF support

--- api/app/api/utils/table_selection.py
+++ api/app/api/utils/table_selection.py
@@ -5,45 +5,49 @@ from .table_details import get_table_schemas
 from .few_shot_examples import get_few_shot_example_messages
 
 
-MSG_WITH_DESCRIPTIONS = (
-    "Return a JSON object with relevant SQL tables for answering the following natural language query: {natural_language_query}"
-    " Respond in JSON format with your answer in a field named \"tables\" which is a list of strings."
-    " Respond with an empty list if you cannot identify any relevant tables."
-    " Write your answer in markdown format."
-    "\n"
-    "The following are descriptions of available tables:\n"
-    "---------------------\n"
-    "{table_details}"
-    "---------------------\n"
-)
+def get_message_with_descriptions():
+    return (
+        "Return a JSON object with relevant SQL tables for answering the following natural language query: {natural_language_query}"
+        " Respond in JSON format with your answer in a field named \"tables\" which is a list of strings."
+        " Respond with an empty list if you cannot identify any relevant tables."
+        " Write your answer in markdown format."
+        "\n"
+        "The following are descriptions of available tables:\n"
+        "---------------------\n"
+        "{table_details}"
+        "---------------------\n"
+    )
+    
 
-DEFAULT_MESSAGES = [
-    {
-        "role": "system",
-        "content": (
-                "You are a helpful assistant for identifying relevant SQL tables to use for answering a natural language query."
-                " You respond in JSON format with your answer in a field named \"tables\" which is a list of strings."
-                " Respond with an empty list if you cannot identify any relevant tables."
-                " Write your answer in markdown format."
-                "\n"
-                "The following are descriptions of available tables:\n"
-                + get_table_schemas()
-            )
-    },
-]
-DEFAULT_MESSAGES.extend(get_few_shot_example_messages("table_selection"))
+def get_default_messages(scope="USA"):
+    default_messages = [
+        {
+            "role": "system",
+            "content": (
+                    "You are a helpful assistant for identifying relevant SQL tables to use for answering a natural language query."
+                    " You respond in JSON format with your answer in a field named \"tables\" which is a list of strings."
+                    " Respond with an empty list if you cannot identify any relevant tables."
+                    " Write your answer in markdown format."
+                    "\n"
+                    "The following are descriptions of available tables:\n"
+                    + get_table_schemas(scope=scope)
+                )
+        },
+    ]
+    default_messages.extend(get_few_shot_example_messages(mode="table_selection", scope=scope))
+    return default_messages
 
 
-def get_relevant_tables(natural_language_query) -> List[str]:
+def get_relevant_tables(natural_language_query, scope="USA") -> List[str]:
     """
     Identify relevant tables for answering a natural language query
     """
-    content = MSG_WITH_DESCRIPTIONS.format(
+    content = get_message_with_descriptions().format(
         natural_language_query=natural_language_query,
-        table_details=get_table_schemas()
+        table_details=get_table_schemas(scope=scope)
         )
 
-    messages = DEFAULT_MESSAGES.copy()
+    messages = get_default_messages(scope=scope).copy()
     messages.append({
         "role": "user",
         "content": content
