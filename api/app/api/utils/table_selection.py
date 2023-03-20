684b19b4794bf4d2df1ed1a159d466c6246048ec
alex <alex@caesarhq.com>
2023-03-20 14:57:22 -0700

    use custom types in prompts

--- api/app/api/utils/table_selection.py
+++ api/app/api/utils/table_selection.py
@@ -12,7 +12,7 @@ def get_message_with_descriptions():
         " Respond with an empty list if you cannot identify any relevant tables."
         " Write your answer in markdown format."
         "\n"
-        "The following are descriptions of available tables:\n"
+        "The following are descriptions of available tables and custom types:\n"
         "---------------------\n"
         "{table_details}"
         "---------------------\n"
@@ -29,7 +29,7 @@ def get_default_messages(scope="USA"):
                     " Respond with an empty list if you cannot identify any relevant tables."
                     " Write your answer in markdown format."
                     "\n"
-                    "The following are descriptions of available tables:\n"
+                    "The following are descriptions of available tables and custom types:\n"
                     + get_table_schemas(scope=scope)
                 )
         },
