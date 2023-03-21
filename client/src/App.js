fe00622f6fc1d64a5b8bae30ef54a6b9968af439
Rahul <rahulsonwalkar23@gmail.com>
2023-03-21 16:50:36 -0700

    [SF GPT] Add query sanitization for SF GPT

--- client/src/App.js
+++ client/src/App.js
@@ -379,9 +379,7 @@ function App(props) {
         clearMapLayers()
 
         // Sanitize the query
-        if(props.version === 'Census') {
-            natural_language_query = cleanupQuery(natural_language_query)
-        }
+        natural_language_query = cleanupQuery(natural_language_query, props.version)
 
         let requestBody = {
             natural_language_query,
