e7a16a64e5daa45756b2d2b39ef8a8fbf06a9b76
Matt Brockman <mattbrockman@gmail.com>
2023-04-10 15:53:48 -0700

    add a privacy policy and tos

---
+++ client/censusGPT/src/privacy.js
@@ -0,0 +1,28 @@
+const PrivacyPolicy = () => {
+    return (
+      <div>
+        <h1>Privacy Policy</h1>
+        <p>CaesarHQ operates the censusGPT.com website, which provides a service for generating SQL queries on census data.</p>
+        <h2>Information Collection and Use</h2>
+        <p>We collect information that you provide when using our website, including the SQL queries that you generate and any feedback that you provide. We use this information to improve our website and provide you with a better user experience.</p>
+        <h2>Log Data</h2>
+        <p>Like many website operators, we collect information that your browser sends whenever you visit our website. This may include information such as your computer's Internet Protocol (IP) address, browser type, browser version, the pages of our website that you visit, the time and date of your visit, the time spent on those pages and other statistics.</p>
+        <h2>Cookies</h2>
+        <p>We use cookies to collect information about your preferences and to personalize your experience on our website. You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent. However, if you do not accept cookies, you may not be able to use some portions of our website.</p>
+        <h2>Third Party Services</h2>
+        <p>We may use third party services, such as Google Analytics, to collect, monitor and analyze usage of our website.</p>
+        <h2>Security</h2>
+        <p>The security of your personal information is important to us, but please remember that no method of transmission over the Internet, or method of electronic storage, is 100% secure. While we strive to use commercially acceptable means to protect your personal information, we cannot guarantee its absolute security.</p>
+        <h2>Changes to Privacy Policy</h2>
+        <p>CaesarHQ reserves the right to modify this privacy policy at any time without notice. Your continued use of our website following any such modification constitutes your agreement to be bound by the modified privacy policy.</p>
+        <h2>Contact Us</h2>
+        <p>If you have any questions about this privacy policy, please contact us at team@caesarhq.com.</p>
+        <p>This privacy policy is effective as of April 10, 2023.</p>
+      </div>
+    );
+  };
+  
+  export default PrivacyPolicy;
+  
+  
+  
