6965c88d931f6d3740b43bbc06828bbadbf24ecc
alex <alex@caesarhq.com>
2023-04-10 16:51:04 -0700

    use sf_analysis_neighborhoods

--- api/app/api/utils/geo_data.py
+++ api/app/api/utils/geo_data.py
@@ -10,5 +10,6 @@ with open("app/data/city_lat_lon.json", "r") as f:
     city_lat_lon = json.load(f)
 
 neighborhood_shapes = {}
-with open("app/data/sf_neighborhoods.json", "r") as f:
+# with open("app/data/sf_neighborhoods.json", "r") as f:
+with open("app/data/sf_analysis_neighborhoods.json", "r") as f:
     neighborhood_shapes = json.load(f)
