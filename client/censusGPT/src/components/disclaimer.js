6c8f2da63d9667265fe7ee28fc49599f52576348
alex <alex@caesarhq.com>
2023-04-12 20:56:55 -0700

    add sf analysis neighborhood explanation in disclaimer

--- client/censusGPT/src/components/disclaimer.js
+++ client/censusGPT/src/components/disclaimer.js
@@ -4,6 +4,8 @@ const Disclaimer = (props) => {
             Disclaimer: SanFranciscoGPT currently only supports data about crime, 311 cases, age, race, gender, income and population in San Francisco. But we are working to add more data! 
             <br />
             <a href="https://data.sfgov.org/City-Infrastructure/311-Cases/vw6y-z8j6" style={{ color: 'blue', textDecoration: 'underline' }}>311 data</a> and <a href="https://data.sfgov.org/Public-Safety/Police-Department-Incident-Reports-2018-to-Present/wg3w-h783" style={{ color: 'blue', textDecoration: 'underline' }}>crime data</a> are sourced from the city's website for public datasets and include data from 1/1/21 to 4/7/23.
+            <br />
+            This app uses <a href="https://data.sfgov.org/Geographic-Locations-and-Boundaries/Analysis-Neighborhoods/p5b7-5n3h" style={{ color: 'blue', textDecoration: 'underline' }}>SF Analysis Neighborhoods</a> which have boundaries formed specifically to fit census tracts.
         </>
     );
 
