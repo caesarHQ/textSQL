0a7f881153d27b75ed51e02345aa50cb31ea0446
alex <alex@caesarhq.com>
2023-04-14 11:07:53 -0700

    update search placeholder

--- client/censusGPT/src/components/searchBar.js
+++ client/censusGPT/src/components/searchBar.js
@@ -34,8 +34,11 @@ const SearchBar = (props) => {
                         name="search"
                         id="search"
                         placeholder={`Ask anything about ${
-                            props.version === "Census" ? "US" : props.version
-                        } Demographics...`}
+                            props.version === "Census" ?
+                                "US crime, demographics, housing..."
+                                :
+                                "San Francisco crime, 311 cases, demographics..."
+                        }`}
                         className="block w-full rounded-none rounded-l-md border-0 py-1.5 ring-1 ring-inset ring-gray-300 dark:ring-neutral-500 focus:ring-2 focus:ring-inset focus:ring-blue-600 dark:focus:ring-blue-600 text-sm leading-6 bg-transparent dark:placeholder-neutral-400"
                         value={value}
                         onChange={onSearchChange}
