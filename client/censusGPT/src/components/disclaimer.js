6a6908f1a669d3dc6aa2dd38a30688d4b2c4f458
alex <alex@caesarhq.com>
2023-04-13 21:23:41 -0700

    update disclaimer

--- client/censusGPT/src/components/disclaimer.js
+++ client/censusGPT/src/components/disclaimer.js
@@ -1,9 +1,9 @@
 const Disclaimer = (props) => {
     const SF_disclaimer = (
         <>
-            Disclaimer: SanFranciscoGPT currently only supports data about crime, 311 cases, age, race, gender, income and population in San Francisco. But we are working to add more data! 
+            Note: SanFranciscoGPT currently only has data for crime, 311 cases, age, race, gender, income and population in SF. But we are working to add more data! 
             <br />
-            <a href="https://data.sfgov.org/City-Infrastructure/311-Cases/vw6y-z8j6" style={{ color: 'blue', textDecoration: 'underline' }}>311 data</a> and <a href="https://data.sfgov.org/Public-Safety/Police-Department-Incident-Reports-2018-to-Present/wg3w-h783" style={{ color: 'blue', textDecoration: 'underline' }}>crime data</a> are sourced from the city's website for public datasets and include data from 1/1/21 to 4/7/23.
+            <a href="https://data.sfgov.org/City-Infrastructure/311-Cases/vw6y-z8j6" style={{ color: 'blue', textDecoration: 'underline' }}>311 data</a> and <a href="https://data.sfgov.org/Public-Safety/Police-Department-Incident-Reports-2018-to-Present/wg3w-h783" style={{ color: 'blue', textDecoration: 'underline' }}>crime data</a> are sourced from the <a href="https://datasf.org/opendata/" style={{ color: 'blue', textDecoration: 'underline' }}>city's website for public datasets</a> and include data from 1/1/21 to 4/7/23.
             <br />
             This app uses <a href="https://data.sfgov.org/Geographic-Locations-and-Boundaries/Analysis-Neighborhoods/p5b7-5n3h" style={{ color: 'blue', textDecoration: 'underline' }}>SF Analysis Neighborhoods</a> which have boundaries formed specifically to fit census tracts.
         </>
@@ -11,7 +11,7 @@ const Disclaimer = (props) => {
 
     const Census_disclaimer = (
         <>
-            Disclaimer: CensusGPT currently only supports data about crime, age, race, gender, income, education levels and population in the USA. But we are working to add more data!
+            Note: CensusGPT currently only has data for crime, age, race, gender, income, education levels and population in the USA. But we are working to add more data!
             <br />
             Census data is sourced from the 2021 ACS (latest). Crime data is sourced from the FBI's 2019 UCR (latest).
         </>
