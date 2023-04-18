9dbb0531701b4c69aa1750fbd2c06dc8a68b322e
Matt Brockman <mattbrockman@gmail.com>
2023-04-18 12:10:45 -0700

    schema management is a separate tab

--- sports/sports-client/app.py
+++ sports/sports-client/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
-from displays.admin_management import admin_management_display
+from displays.credentials_management import admin_management_display
 from displays.query_datasource import query_datasource_display
+from displays.schema_management import schema_management_display
 from config import API_BASE
 
 
@@ -9,11 +10,16 @@ def main():
     is_localhost = 'localhost' in API_BASE
 
     if is_localhost:
-        state = st.sidebar.radio("Change Mode", ["Admin", "Query"])
+        state = st.sidebar.radio(
+            "Change Mode", ["Credentials", "Schema Management", "Query"])
     else:
         state = 'Query'
-    if state == "Admin":
+    if state == "Credentials":
         admin_management_display()
+
+    if state == "Schema Management":
+        schema_management_display()
+
     if state == "Query":
         query_datasource_display()
 
--- sports/sports-client/displays/credentials_management.py
+++ sports/sports-client/displays/credentials_management.py
@@ -38,14 +38,6 @@ def update_table_checked(name, *args):
             table['active'] = isChecked
 
 
-def update_column_checked(columnIdx, table, *args):
-    # it's {name: str, columns: str[], active: bool}
-    isChecked = args[0] if args else False
-    for table in st.session_state["tables"]:
-        if table.get('name') == table:
-            table['columns'][columnIdx]['active'] = isChecked
-
-
 def admin_management_display():
     st.title("Set up your Database")
     if "tables" not in st.session_state:
@@ -110,53 +102,6 @@ def admin_management_display():
             # when checked, find the table and set the active value to false/true
             st.checkbox(
                 'Table: ' + table.get('name'), value=is_checked, key=table.get('name'), on_change=functools.partial(update_table_checked, name=table.get('name')))
-
-    # for each of the tables, show the scheama. Later it should be table columns + schema but one thing at a time.
-
-    for idx, table in enumerate(st.session_state["tables"]):
-
-        if table.get('active', False):
-            # expandable table
-            with st.expander(table.get('name')):
-
-                st.subheader(table.get('name'))
-
-                # have a Table label, a text area with the value of the table.schema, and a button to generate the schema, and a button to save the schema
-                for column in table.get('columns', []):
-                    column_checked = column.get('active', False)
-                    st.checkbox(
-                        '---Column: ' + column.get('name') + ', ' + column.get('type'), value=column_checked, key=table.get('name') + '_' + column.get('name'), on_change=functools.partial(update_column_checked, columnIdx=table.get('columns').index(column), table=table.get('name')))
-
-                if st.button("Generate Schema", key="generate_schema_" + table.get('name')):
-                    # send the values to the backend to set up the database
-                    response = requests.post(f"{ADMIN_BASE}/generate_schema",
-                                             json=table)
-                    response = response.json()
-                    if response.get('status') == 'success':
-                        new_sql = response.get('message')
-                        if new_sql:
-                            table['schema'] = new_sql
-                            st.session_state["tables"][idx]['schema'] = new_sql
-                            st.session_state["tables"][idx]['update_count'] = table.get(
-                                'update_count', 0) + 1
-
-                    else:
-                        st.error(response.get('error'))
-                st.text_area(
-                    label="Schema", label_visibility="hidden", value=table.get('schema', ''), height=100, key="schema_" + table.get('name'))
-
-    # expandable for enums
-    with st.expander("Enums"):
-        st.subheader("Enums")
-
-        if st.button("Refresh Enums", key="refresh_enums"):
-            # send the values to the backend to set up the database
-            response = requests.get(f"{ADMIN_BASE}/load_enums")
-            response = response.json()
-            if response.get('status') == 'success':
-                st.success(response.get('enums'))
-            else:
-                st.error(response.get('error'))
 
     # add a save button
     if st.button("Save", key="save_tables"):
