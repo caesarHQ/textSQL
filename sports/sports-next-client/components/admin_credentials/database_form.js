86c78ce431188db46a71ff5a6d57bc5100304aa2
Matt Brockman <mattbrockman@gmail.com>
2023-04-22 01:07:51 -0700

    form styling

--- sports/sports-next-client/components/admin_credentials/database_form.js
+++ sports/sports-next-client/components/admin_credentials/database_form.js
@@ -4,10 +4,14 @@ export const DatabaseConfigComponent = () => {
   const [config, setConfig] = useState("url");
 
   return (
-    <div>
-      <DatabaseConfigPicker config={config} setConfig={setConfig} />
-      {config === "url" && <DatabaseURLForm />}
-      {config === "fields" && <DatabaseFieldForm />}
+    <div className="border border-gray-300 p-4 rounded">
+      <div className="mb-4">
+        <DatabaseConfigPicker config={config} setConfig={setConfig} />
+      </div>
+      <div className="border-t border-gray-300 pt-4">
+        {config === "url" && <DatabaseURLForm />}
+        {config === "fields" && <DatabaseFieldForm />}
+      </div>
     </div>
   );
 };
@@ -49,14 +53,14 @@ const DatabaseURLForm = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className="mb-4">
-        <label htmlFor="url" className="block mb-2 font-bold">
+      <div className="flex items-center mb-4">
+        <label htmlFor="url" className="block mr-2 font-bold">
           URL
         </label>
         <input
           type="text"
           id="url"
-          className="w-full border border-gray-400 p-2 rounded"
+          className="w-3/4 border border-gray-400 p-2 rounded"
           placeholder="postgresql://username:password@host:port/database"
           value={url}
           onChange={handleUrlChange}
@@ -71,7 +75,6 @@ const DatabaseURLForm = () => {
     </form>
   );
 };
-
 const DatabaseFieldForm = () => {
   const [host, setHost] = useState("");
   const [username, setUsername] = useState("");
@@ -103,65 +106,73 @@ const DatabaseFieldForm = () => {
     event.preventDefault();
     // TODO: Handle form submission
   };
-
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="mb-4">
-        <label htmlFor="host" className="block mb-2 font-bold">
-          Host URL
-        </label>
-        <input
-          type="text"
-          id="host"
-          className="w-full border border-gray-400 p-2 rounded"
-          value={host}
-          onChange={handleHostChange}
-        />
-      </div>
-      <div className="mb-4">
-        <label htmlFor="username" className="block mb-2 font-bold">
-          Username
+    <form onSubmit={handleSubmit} className="justify-start">
+      <div className="flex items-center mb-4">
+        <label
+          htmlFor="username"
+          className="block mr-2 font-bold w-24 text-right"
+        >
+          Username:
         </label>
         <input
           type="text"
           id="username"
-          className="w-full border border-gray-400 p-2 rounded"
+          className="border border-gray-400 p-2 rounded w-64"
           value={username}
           onChange={handleUsernameChange}
         />
       </div>
-      <div className="mb-4">
-        <label htmlFor="password" className="block mb-2 font-bold">
-          Password
+      <div className="flex items-center mb-4">
+        <label
+          htmlFor="password"
+          className="block mr-2 font-bold w-24 text-right"
+        >
+          Password:
         </label>
         <input
           type="password"
           id="password"
-          className="w-full border border-gray-400 p-2 rounded"
+          className="border border-gray-400 p-2 rounded w-64"
           value={password}
           onChange={handlePasswordChange}
         />
       </div>
-      <div className="mb-4">
-        <label htmlFor="port" className="block mb-2 font-bold">
-          Port
+      <div className="flex items-center mb-4">
+        <label htmlFor="host" className="block mr-2 font-bold w-24 text-right">
+          Host:
+        </label>
+        <input
+          type="text"
+          id="host"
+          className="border border-gray-400 p-2 rounded w-64"
+          value={host}
+          onChange={handleHostChange}
+        />
+      </div>
+      <div className="flex items-center mb-4">
+        <label htmlFor="port" className="block mr-2 font-bold w-24 text-right">
+          Port:
         </label>
         <input
           type="text"
           id="port"
-          className="w-full border border-gray-400 p-2 rounded"
+          className="border border-gray-400 p-2 rounded w-64"
           value={port}
           onChange={handlePortChange}
         />
       </div>
-      <div className="mb-4">
-        <label htmlFor="database" className="block mb-2 font-bold">
-          Database Name
+      <div className="flex items-center mb-4">
+        <label
+          htmlFor="database"
+          className="block mr-2 font-bold w-24 text-right"
+        >
+          Database:
         </label>
         <input
           type="text"
           id="database"
-          className="w-full border border-gray-400 p-2 rounded"
+          className="border border-gray-400 p-2 rounded w-64"
           value={database}
           onChange={handleDatabaseChange}
         />
