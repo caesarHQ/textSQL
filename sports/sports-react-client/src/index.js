465e2fe3eab915c15cea06a08d5b449d73740bd3
Matt Brockman <mattbrockman@gmail.com>
2023-04-21 23:09:52 -0700

    add tailwind

--- sports/sports-react-client/src/index.js
+++ sports/sports-react-client/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
---
+++ sports/sports-react-client/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: false,
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
