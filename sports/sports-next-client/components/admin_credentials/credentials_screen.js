f865360a4cf9b25424ecc4dc00e23c32fae770a2
Matt Brockman <mattbrockman@gmail.com>
2023-04-22 00:39:39 -0700

    closable rows component

--- sports/sports-next-client/components/admin_credentials/credentials_screen.js
+++ sports/sports-next-client/components/admin_credentials/credentials_screen.js
@@ -1,7 +1,12 @@
+import { ClosableRow } from "../closable_row";
+
 const CredentialsScreen = () => {
   return (
     <div>
       <h1>Admin Credentials</h1>
+      <ClosableRow title="Database">
+        <div>Database stuff</div>
+      </ClosableRow>
     </div>
   );
 };
---
+++ sports/sports-next-client/components/closable_row.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faChevronUp, faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+library.add(faChevronUp, faChevronDown);
+
+export const ClosableRow = ({ title, children, className, ...props }) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className={className} {...props}>
+      <div
+        className="flex items-center cursor-pointer"
+        onClick={() => setOpen(!open)}
+      >
+        <div className="flex items-center">
+          <div className="text-2xl">
+            {open ? (
+              <FontAwesomeIcon icon={faChevronUp} />
+            ) : (
+              <FontAwesomeIcon icon={faChevronDown} />
+            )}
+          </div>
+          <div className="ml-2 mr-2">{open ? "Hide" : "Show"}</div>
+        </div>
+
+        <h3 className="text-lg font-bold">{title}</h3>
+      </div>
+      {open && <div className="mt-4">{children}</div>}
+    </div>
+  );
+};
