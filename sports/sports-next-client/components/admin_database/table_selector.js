fcbe0c6019fe722c1a82326c0db3466c5bd1ef05
Matt Brockman <mattbrockman@gmail.com>
2023-04-22 22:35:45 -0700

    actually save the table selection

--- sports/sports-next-client/components/admin_database/table_selector.js
+++ sports/sports-next-client/components/admin_database/table_selector.js
@@ -2,7 +2,45 @@ import { useContext } from "react";
 import { AdminContext } from "@/contexts/admin_context";
 
 export const TableSelector = () => {
-  const { tables } = useContext(AdminContext);
+  const { tables, setTables } = useContext(AdminContext);
 
-  return <div>{JSON.stringify(tables)}</div>;
+  const updateTable = (idx, newTable) => {
+    const newTables = [...tables];
+    newTables[idx] = newTable;
+    setTables(newTables);
+  };
+
+  return (
+    <div>
+      {tables?.map((table, idx) => {
+        return (
+          <SingleTableSelection
+            table={table}
+            setTable={(newTable) => updateTable(idx, newTable)}
+            key={idx}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+const SingleTableSelection = ({ table, setTable }) => {
+  return (
+    <div className="flex flex-row items-center justify-between w-200px">
+      <div onClick={() => setTable({ ...table, active: !table.active })}>
+        <input
+          type="checkbox"
+          name="table"
+          checked={table.active}
+          onChange={(event) =>
+            setTable({ ...table, active: event.target.checked })
+          }
+        />
+        <label className="ml-2" htmlFor="table">
+          {table.name}
+        </label>
+      </div>
+    </div>
+  );
 };
