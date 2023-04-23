f09cf125cf5c4bef01b5d4420d7f05f6b1790918
Matt Brockman <mattbrockman@gmail.com>
2023-04-22 17:56:38 -0700

    fix linter for main app

--- client/censusGPT/src/components/results/resultsContainer.js
+++ client/censusGPT/src/components/results/resultsContainer.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react'
+import { useState } from 'react'
 import { DataVisualization } from './dataVisualization'
 import { SQLDisplay } from './sqlDisplay'
 import Table from '../table'
