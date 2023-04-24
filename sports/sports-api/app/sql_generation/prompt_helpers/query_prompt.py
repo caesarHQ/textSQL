176135f016915740260155c45632de1f218724b5
Matt Brockman <mattbrockman@gmail.com>
2023-04-24 15:04:25 -0700

    additional prompt on the table relations

--- sports/sports-api/app/sql_generation/prompt_helpers/query_prompt.py
+++ sports/sports-api/app/sql_generation/prompt_helpers/query_prompt.py
@@ -10,10 +10,11 @@ Provide a properly formatted JSON object with the following information. Ensure 
 
     "Required Answer": str, required (the type of information the query is asking for),
     "Input Types": str, required (a summary of the enums or other conversion that are related to the query),
-    "Plan": str, required (Explain the simplest way to answer the question/command with the schemas available),
+    "Plan": str, required (a step by step walk thru of the joins/filters to be done and how to ensure uniqueness/deduplication),
     "Additional Data to Get: str[], required (brainstorm what information related to the original query should also be returned to answer the question/command.),
     "SQL": str, required (formatted '''line1\\nnext line\\netc''')
 }}
 
-Provide the JSON and only the JSON. It should be formatted for parsing in Python. Ensure everything is properly escaped for parsing and executing.
+Provide the JSON and only the JSON. It should be formatted for parsing in Python. Ensure everything inside the SQL statement is properly escaped for parsing and executing (ensure \n is \\n, \m is \\m etc).
+Do not include any variables/wildcards.
 """.format(command)
--- sports/sports-api/app/table_selection/utils.py
+++ sports/sports-api/app/table_selection/utils.py
@@ -172,6 +172,7 @@ def _get_table_selection_message_with_descriptions(natural_language_query):
             "Rephrased Input": string (any assumptions about words in the input and what they refer to)
             "required answer": string[] (the final variables that will be needed)
             "input conversions": string[] (the variables/tables that will be needed to interpret the input)
+            "internal relations": string (describe how the required tables relate to one another and how to make sure relevant information is not lost)
             "reasoning": string (Reverse walkthrough from end to start where the information will come from (what joins are needed). Column B.A gives Y, but B doesn't have Z we need to pull D.A to get Z.))
             "double_check": string (Walking through the tables mentioned above, check that each column that will be used to find any missing columns, add any additional tables that could be useful)
             "tables": string[]
