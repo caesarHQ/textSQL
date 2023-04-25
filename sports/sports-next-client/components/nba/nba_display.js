e657d6b939ca5be674024d5f59f4fc8c42448e6b
Matt Brockman <mattbrockman@gmail.com>
2023-04-25 14:17:43 -0700

    date picker and filter games on day

--- sports/sports-next-client/components/nba/nba_display.js
+++ sports/sports-next-client/components/nba/nba_display.js
@@ -1,17 +1,37 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext, useMemo } from "react";
 import { getGames } from "@/apis/sports_apis";
 import { NbaContext } from "./nba_context";
 import { NBAGameRowDisplay } from "./cards/nba_game_row_display";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 export const NbaDisplay = () => {
   const { currentGames, setCurrentGames, teamLookup } = useContext(NbaContext);
-  const [currentDate, setCurrentDate] = useState({
-    month: 3,
-    year: 2021,
-  });
+  //start on March 1st, 2021
+  const [currentDate, setCurrentDate] = useState(new Date(2021, 2, 1));
+
+  const currentYear = currentDate.getFullYear();
+  const currentMonth = currentDate.getMonth() + 1;
+
+  const todaysGames = useMemo(() => {
+    return currentGames.filter((game) => {
+      //check if the currentGame date is the same as the currentDate. game_et is in the format of "Fri, 02 Apr 2021 19:30:00 GMT"
+      const gameDate = new Date(game.game_et);
+      return (
+        gameDate.getFullYear() === currentYear &&
+        gameDate.getMonth() + 1 === currentMonth &&
+        gameDate.getDate() === currentDate.getDate()
+      );
+    });
+  }, [currentGames, currentDate, currentYear, currentMonth]);
 
   const updateCurrentGames = async () => {
-    const newGames = await getGames({ ...currentDate });
+    const dateDict = {
+      year: currentDate.getFullYear(),
+      month: currentDate.getMonth() + 1,
+    };
+
+    const newGames = await getGames({ ...dateDict });
 
     if (newGames.status === "success") {
       let resultant_games = newGames.games;
@@ -31,13 +51,19 @@ export const NbaDisplay = () => {
 
   useEffect(() => {
     updateCurrentGames();
-  }, []);
+  }, [currentYear, currentMonth]);
 
   return (
     <div className="flex flex-col items-center justify-center">
       <h1>Sports</h1>
+      <div className="flex flex-row items-center justify-center">
+        <DatePicker
+          selected={currentDate}
+          onChange={(date) => setCurrentDate(date)}
+        />
+      </div>
       <div className="flex flex-col items-center justify-center">
-        {currentGames?.map((game) => {
+        {todaysGames?.map((game) => {
           return (
             <NBAGameRowDisplay
               game={game}
