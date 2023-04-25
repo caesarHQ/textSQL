2b572bfcd71d445d7f4678ab9c6994299d0b6c79
Matt Brockman <mattbrockman@gmail.com>
2023-04-24 20:21:43 -0700

    start framework for games page

---
+++ sports/sports-next-client/apis/sports_apis.js
@@ -0,0 +1,6 @@
+let api_endpoint = "http://localhost:9000";
+
+export const getGames = async ({ month, year }) => {
+  console.log("hello fren");
+  return { games: [1, 2, 3], status: "success" };
+};
---
+++ sports/sports-next-client/pages/sports.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import { getGames } from "@/apis/sports_apis";
+
+const Sports = () => {
+  const [currentGames, setCurrentGames] = useState([]);
+  const [currentDate, setCurrentDate] = useState({
+    month: 1,
+    year: 2020,
+  });
+
+  const updateCurrentGames = async () => {
+    const newGames = await getGames({ ...currentDate });
+
+    if (newGames.status === "success") {
+      setCurrentGames(newGames.games);
+    }
+  };
+
+  console.log("currentGames: ", currentGames);
+
+  useEffect(() => {
+    updateCurrentGames();
+  }, [currentDate]);
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1>Sports</h1>
+      <div className="flex flex-row items-center justify-center">
+        {currentGames.map((game, idx) => {
+          return (
+            <div
+              className="flex flex-col items-center justify-center"
+              key={idx}
+            >
+              <p>{JSON.stringify(game)}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+export default Sports;
