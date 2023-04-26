c26697a6aa1195cf9e325d8d32dc87f4824ae0c3
Matt Brockman <mattbrockman@gmail.com>
2023-04-25 21:09:43 -0700

    show basic game stats

--- sports/sports-next-client/components/nba/game_data/game_page.js
+++ sports/sports-next-client/components/nba/game_data/game_page.js
@@ -34,21 +34,77 @@ const GamePage = ({ id }) => {
   return (
     <div className="flex flex-col items-center justify-center w-full">
       <div>Game: {id}</div>
-      <BasicGameDisplay
-        game={myGame}
-        teamLookup={teamLookup}
-        team={myGame.away_team_id}
-      />
+      {!!myGameStats && (
+        <BasicGameDisplay stats={myGameStats} teamLookup={teamLookup} />
+      )}
     </div>
   );
 };
 
 export default GamePage;
 
-const BasicGameDisplay = ({ game, teamLookup, team }) => {
+const BasicGameDisplay = ({ stats, teamLookup, team }) => {
   return (
     <div className="flex flex-col items-center justify-center w-full">
-      {JSON.stringify(teamLookup[team])}
+      <ScoreBox stats={stats} teamLookup={teamLookup} />
     </div>
+  );
+};
+
+const ScoreBox = ({ stats, teamLookup }) => {
+  const teams = Object.keys(stats || {});
+  console.log("stats: ", stats);
+  console.log("teams: ", teams);
+
+  return (
+    <table className="w-full bg-white rounded-lg">
+      <thead className="bg-gray-100">
+        <tr>
+          <ScoreBoxHead label="Team" />
+          <ScoreBoxHead label="Points" />
+          <ScoreBoxHead label="Assists" />
+          <ScoreBoxHead label="Rebounds" />
+          <ScoreBoxHead label="Steals" />
+          <ScoreBoxHead label="3-Pointers Made" />
+          <ScoreBoxHead label="FG%" />
+          <ScoreBoxHead label="FT%" />
+        </tr>
+      </thead>
+      <tbody>
+        {teams
+          ?.map((t) => stats[t])
+          .map((t) => {
+            return (
+              <tr key={t.team_id}>
+                <ScoreBoxData
+                  data={teamLookup[t.team_id]?.name}
+                  width={"1/3"}
+                />
+                <ScoreBoxData data={t.points} />
+                <ScoreBoxData data={t.assists} />
+                <ScoreBoxData data={t.rebounds_total} />
+                <ScoreBoxData data={t.steals} />
+                <ScoreBoxData data={t.three_pointers_made} />
+                <ScoreBoxData data={t.field_goals_percentage} />
+                <ScoreBoxData data={t.free_throws_percentage} />
+              </tr>
+            );
+          })}
+      </tbody>
+    </table>
+  );
+};
+
+const ScoreBoxHead = ({ label, width = "1/6" }) => {
+  return (
+    <th className={`w-${width} p-4 text-center font-medium text-gray-700`}>
+      {label}
+    </th>
+  );
+};
+
+const ScoreBoxData = ({ data }) => {
+  return (
+    <td className="w-1/6 p-4 text-center font-medium text-gray-700">{data}</td>
   );
 };
