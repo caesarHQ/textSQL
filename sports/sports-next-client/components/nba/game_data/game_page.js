1fa7477954a816ea5d2f95716b36778dce15dd1d
Matt Brockman <mattbrockman@gmail.com>
2023-04-25 17:01:31 -0700

    can't reuse the component that needs context ujnless move it up higher

--- sports/sports-next-client/components/nba/game_data/game_page.js
+++ sports/sports-next-client/components/nba/game_data/game_page.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { NBAGameRowDisplay } from "@/components/nba/cards/nba_game_row_display";
 import { getGameById } from "@/apis/sports_apis";
 
 const GamePage = ({ id }) => {
@@ -18,9 +17,6 @@ const GamePage = ({ id }) => {
   return (
     <div className="flex flex-col items-center justify-center w-full">
       <div>Game: {id}</div>
-      <div className="flex flex-row items-center justify-center">
-        {myGame?.game_id && <NBAGameRowDisplay game={myGame} />}
-      </div>
     </div>
   );
 };
