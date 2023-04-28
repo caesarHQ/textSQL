5ceaa7053351377600c1c19cf2afa31dc4a19641
Matt Brockman <mattbrockman@gmail.com>
2023-04-28 14:40:35 -0700

    unify the stream endpoint

--- sports/sports-api/app/sql_generation/generation_routes.py
+++ sports/sports-api/app/sql_generation/generation_routes.py
@@ -15,54 +15,14 @@ def text_to_sql():
     """
     Convert natural language query to SQL
     """
-    print('checking tables')
-    request_body = request.get_json()
-    natural_language_query = request_body.get("natural_language_query")
-    table_names = request_body.get("table_names")
-
-    if not natural_language_query:
-        error_msg = "`natural_language_query` is missing from request body"
-        return make_response(jsonify({"error": error_msg}), 400)
 
     try:
-        if not table_names:
-            if PINECONE_ENV and PINECONE_KEY:
-                table_names = get_relevant_tables_from_pinecone(
-                    natural_language_query)
-            else:
-
-                table_names = get_relevant_tables_from_lm(
-                    natural_language_query, ignore_comments=True)
-
-        result, sql_query = text_to_sql_with_retry(
-            natural_language_query, table_names)
-    except Exception as e:
-        error_msg = f"Error processing request: {str(e)}"
-        return make_response(jsonify({"error": error_msg}), 500)
-
-    return make_response(jsonify({"result": result, "sql_query": sql_query}), 200)
-
-
-@bp.route('/text_to_sql_streaming', methods=['POST'])
-def text_to_sql_streaming():
-    """
-    Convert natural language query to SQL
-    """
-
-    request_body = request.get_json()
-
-    return Response(stream_with_context(streaming_helper.stream_sql_response(request_body)), content_type="application/json")
-
-
-@bp.route('/text_to_sql_unified', methods=['POST'])
-def text_to_sql_unified():
-    """
-    Convert natural language query to SQL
-    """
-
-    request_body = request.get_json()
+        request_body = request.get_json()
+    except:
+        return make_response(jsonify({"status": "error", "error": "Error parsing request body. Params:\nnatural_language_query, scope"}), 400)
 
     if "stream" in request_body:
+        # this might not be the best way to do it, will need to check if there's a more official way using the regular stream decorator
         return Response(stream_with_context(streaming_helper.stream_sql_response(request_body)), content_type="application/json")
 
     else:
