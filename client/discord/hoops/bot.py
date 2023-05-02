4e64f66a8ac2f2374b51dd04ec0a2a62ffb430e9
Rahul <rahulsonwalkar23@gmail.com>
2023-05-01 21:54:34 -0700

    Send thread id and session id to the backend

--- client/discord/hoops/bot.py
+++ client/discord/hoops/bot.py
@@ -94,7 +94,7 @@ async def on_message(message):
             # Create a thread 
             thread = await final_bot_response.create_thread(name=natural_language_query, auto_archive_duration=60)
 
-            register_thread_session(thread.id, response['session_id'])
+            register_thread_session_to_backend(thread.id, response['session_id'])
 
             # Send raw data as csv in thread
             await send_raw_data_as_csv(response["response"], thread)
@@ -106,8 +106,13 @@ async def on_message(message):
         print(f"Error in on_message: {e}")
         await message.channel.send(f"Sorry, something went wrong. \n {e}")
 
-def register_thread_session(thread_id, session_id):
-    # register thread and session
+def register_thread_session_to_backend(thread_id, session_id):
+    url = "https://text-sql-be.onrender.com/register_thread"
+
+    payload = {"thread_id": thread_id, "session_id": session_id}
+    headers = {"Content-Type": "application/json"}
+
+    _ = requests.post(url, json=payload, headers=headers)
     return 
 
 async def handle_help(message):
